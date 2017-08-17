8fdfae84fa11140496f3b7ceb8d4c8c1901f356e
Caroline Bowes <carvbowes@gmail.com>
2017-08-17 01:35:17 -0400

    changed default friends'

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,45 +5,45 @@ var friends = [
 	  	scores:[
 	      5,
 	      1,
-	      4,
-	      4,
-	      5,
 	      1,
+	      1,
+	      3,
+	      5,
+	      5,
 	      2,
 	      5,
-	      4,
-	      1
+	      5
 	    ]
 	},	{
 
 	  	name:"Meeko",
 	  	photo:"https://i.pinimg.com/originals/c6/f6/7c/c6f67ca5c821e49eb45dd5e65548fc32.jpg",
 	  	scores:[
-	      5,
+	      3,
 	      1,
-	      4,
-	      4,
 	      5,
-	      1,
 	      2,
+	      2,
+	      1,
 	      5,
-	      4,
-	      1
+	      5,
+	      2,
+	      5
 	    ]
 	}, 	{
 	  	name:"Iago",
 	  	photo:"http://i.dailymail.co.uk/i/pix/2016/10/04/11/391554F100000578-0-image-a-1_1475575896332.jpg",
 	  	scores:[
-	      5,
+	      2,
 	      1,
 	      4,
+	      3,
 	      4,
-	      5,
 	      1,
 	      2,
-	      5,
-	      4,
-	      1
+	      2,
+	      1,
+	      2
 	    ]
 	}
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,6 @@
 var friends = require("../data/friends.js");
 
 module.exports = function(app) {
-	console.log(friends);
 	app.get("/api/friends", function(req, res) {
 		res.json(friends);
 	});
